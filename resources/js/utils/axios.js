f54e38af100a4229fe53141874aaec783a751ece
akbarsaladin36 <asong36@gmail.com>
2026-03-09 07:01:49 +0700

    rebuild assets part 1

--- resources/js/utils/axios.js
+++ resources/js/utils/axios.js
@@ -1,37 +1,39 @@
-import axios from "axios"
+import axios from "axios";
+
+console.log("CEK URL API : ", import.meta.env.VITE_API_BASE_URL);
 
 const axiosApi = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    withCredentials: true
-})
+    baseURL: import.meta.env.VITE_API_BASE_URL || "/api/v1",
+    withCredentials: true,
+});
 
 axiosApi.interceptors.request.use(
-  function (config) {
-    return config
-  },
-  function (error) {
-    return Promise.reject(error)
-  }
-)
+    function (config) {
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    },
+);
 
 axiosApi.interceptors.response.use(
-  function (response) {
-    return response
-  },
-  function (error) {
-    if(error.response) {
-        if (error.response.status === 401) {
-          console.log(error.response)
+    function (response) {
+        return response;
+    },
+    function (error) {
+        if (error.response) {
+            if (error.response.status === 401) {
+                console.log(error.response);
+            }
+            if (error.response.status === 403) {
+                console.log(error.response);
+            }
+            if (error.response.status === 500) {
+                console.log(error.response);
+            }
         }
-        if (error.response.status === 403) {
-          console.log(error.response)
-        }
-        if (error.response.status === 500) {
-          console.log(error.response)
-        }
-    }
-    return Promise.reject(error)
-  }
-)
+        return Promise.reject(error);
+    },
+);
 
-export default axiosApi
+export default axiosApi;
