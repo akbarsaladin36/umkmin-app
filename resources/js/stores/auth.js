3e222ae7773c68576f89c2dac6155aaa74daa432
akbarsaladin36 <asong36@gmail.com>
2026-03-09 09:02:05 +0700

    update auth function in zustand and rebuild aseets part 5

--- resources/js/stores/auth.js
+++ resources/js/stores/auth.js
@@ -1,47 +1,45 @@
 import { create } from "zustand";
 import axiosApi from "../utils/axios";
 
-const useAuthStore = create(
-    (set) => ({
-        user: null,
-        loading: false,
-        authChecked: false,
-        login: async (data) => {
-            set({ loading: true })
-            try {
-                await axiosApi.post('/auth/login', data)
-                const res = await axiosApi.get('/me')
-                set({
-                    user: res.data.data,
-                    loading: false,
-                    authChecked: true
-                })
-                return res.data.data
-            } catch(error) {
-                console.log(error)
-                set({ user: null, loading: false, authChecked: false })
-            }
-        },
-        logout: async () => {
-            set({ loading: true })
-            try {
-                await axiosApi.post('/auth/logout')
-                set({ user: null, loading: false, authChecked: false })
-            } catch(error) {
-                console.log(error)
-                set({ loading: false })
-            }
-        },
-        fetchUser: async () => {
-            try {
-                const res = await axiosApi.get('/me')
-                set({ user: res.data.data, authChecked: true }) 
-            } catch(error) {
-                console.log(error)
-                set({ user: null })
-            }
+const useAuthStore = create((set) => ({
+    user: null,
+    loading: false,
+    authChecked: false,
+    login: async (data) => {
+        set({ loading: true });
+        try {
+            await axiosApi.post("/auth/login", data);
+            const res = await axiosApi.get("/me");
+            set({
+                user: res.data.data,
+                loading: false,
+                authChecked: true,
+            });
+            return res.data.data;
+        } catch (error) {
+            console.log(error);
+            set({ user: null, loading: false, authChecked: false });
         }
-    })
-)
+    },
+    logout: async () => {
+        set({ loading: true });
+        try {
+            await axiosApi.post("/auth/logout");
+            set({ user: null, loading: false, authChecked: false });
+        } catch (error) {
+            console.log(error);
+            set({ loading: false });
+        }
+    },
+    fetchUser: async () => {
+        try {
+            const res = await axiosApi.get("/me");
+            set({ user: res.data.data, authChecked: true });
+        } catch (error) {
+            console.log(error);
+            set({ user: null, authChecked: true });
+        }
+    },
+}));
 
-export default useAuthStore
+export default useAuthStore;
