e9839a683bb5c2858ac904454740ec3be33e1db8
akbarsaladin36 <asong36@gmail.com>
2026-03-09 19:27:08 +0700

    rebuild assets part 11

--- resources/js/routes/RoleRoute.jsx
+++ resources/js/routes/RoleRoute.jsx
@@ -1,18 +1,18 @@
-import { Navigate, Outlet } from "react-router-dom"
-import useAuthStore from "../stores/auth"
+import { Navigate, Outlet } from "react-router-dom";
+import useAuthStore from "../stores/auth";
 
 const RoleRoute = ({ allowedRoles }) => {
-    const user = useAuthStore((state) => state.user)
+    const user = useAuthStore((state) => state.user);
 
-    if(!user) {
-        return <Navigate to="/" replace/>
+    if (!user) {
+        return <Navigate to="/" replace />;
     }
 
-    if(!allowedRoles.includes(user?.role_id)) {
-        return <Navigate to="/" replace/>
+    if (!allowedRoles.includes(Number(user?.role_id))) {
+        return <Navigate to="/" replace />;
     }
 
-    return <Outlet/>
-}
+    return <Outlet />;
+};
 
-export default RoleRoute
+export default RoleRoute;
